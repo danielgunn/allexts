f8261877e2e052682a56b37b820264506893ef68
Daniel Gunn <dangunn@gmail.com>
2014-11-22 14:40:40 +0800

    path is parameterized

--- allexts.py
+++ allexts.py
@@ -2,7 +2,7 @@ import sys
 import os
 rootpath=sys.argv[1]
 allexts=set('')
-for root, dirs, names in os.walk("D:/baiduyun/Assessments"):
+for root, dirs, names in os.walk(rootpath):
 	for name in names:
 		ext = os.path.splitext(name)[1][1:]
 		allexts.add(ext)
